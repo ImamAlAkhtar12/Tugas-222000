d0c0f2c796f7c1b070b0be597edf76c95a398981
ImamAlAkhtar12 <140218569+ImamAlAkhtar12@users.noreply.github.com>
2023-08-17 18:10:20 +0700

    Add files via upload

---
+++ tugas4.py
@@ -0,0 +1,3 @@
+print("NAMA SAYA:IMAM AL AKHTAR")
+print("SAYA DARI KELAS X-RPL1")
+print("gambaran saya untuk jurusan rpl ini adalah di jurusan rpl ini saya akan di ajari untuk membuat suatu program baik itu program untuk membuat suatu aplikasi dll dan juga gambaran saya untuk menjadi apa di jurusan rpl ini adalah saya ingin menjadi pengusaha yang mungkin nanti usaha yang saya lakukan ada sangkut pautnya dengan apa yang saya pelajari di rpl ini")
